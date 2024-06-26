a80bc2a99d69e5a64216bdbef19ebca3b3065273
Truong-T-Phong <trggphong@gmail.com>
2024-06-26 17:48:16 +0700

    fix text , UI

--- lib/features/location/domain/reposotories/location_repo.dart
+++ lib/features/location/domain/reposotories/location_repo.dart
@@ -14,12 +14,15 @@ class LocationRepo implements LocationRepoInterface {
 
   @override
   Future<ZoneResponseModel> getZone(String? lat, String? lng) async {
-    Response response = await apiClient.getData('${AppConstants.zoneUri}?lat=$lat&lng=$lng', handleError: false);
-    if(response.statusCode == 200) {
+    Response response = await apiClient.getData(
+        '${AppConstants.zoneUri}?lat=$lat&lng=$lng',
+        handleError: false);
+    if (response.statusCode == 200) {
       ZoneResponseModel responseModel;
       List<int>? zoneIds = ZoneModel.fromJson(response.body).zoneIds;
       List<ZoneData>? zoneData = ZoneModel.fromJson(response.body).zoneData;
-      responseModel = ZoneResponseModel(true, '' , zoneIds ?? [], zoneData??[]);
+      responseModel =
+          ZoneResponseModel(true, '', zoneIds ?? [], zoneData ?? []);
       return responseModel;
     } else {
       return ZoneResponseModel(false, response.statusText, [], []);
@@ -28,11 +31,12 @@ class LocationRepo implements LocationRepoInterface {
 
   @override
   Future<String> getAddressFromGeocode(LatLng latLng) async {
-    Response response = await apiClient.getData('${AppConstants.geocodeUri}?lat=${latLng.latitude}&lng=${latLng.longitude}');
-    String address = 'Unknown Location Found';
-    if(response.statusCode == 200 && response.body['status'] == 'OK') {
+    Response response = await apiClient.getData(
+        '${AppConstants.geocodeUri}?lat=${latLng.latitude}&lng=${latLng.longitude}');
+    String address = 'Unknown Location Found'.tr;
+    if (response.statusCode == 200 && response.body['status'] == 'OK') {
       address = response.body['results'][0]['formatted_address'].toString();
-    }else {
+    } else {
       showCustomSnackBar(response.body['error_message'] ?? response.bodyString);
     }
     return address;
@@ -47,14 +51,15 @@ class LocationRepo implements LocationRepoInterface {
   //   }
   // }
 
-
   @override
   Future<Response> searchLocation(String text) async {
-    return await apiClient.getData('${AppConstants.searchLocationUri}?search_text=$text');
+    return await apiClient
+        .getData('${AppConstants.searchLocationUri}?search_text=$text');
   }
 
   Future<Response> getById(int id) async {
-    Response response = await apiClient.getData('${AppConstants.placeDetailsUri}?placeid=$id');
+    Response response =
+        await apiClient.getData('${AppConstants.placeDetailsUri}?placeid=$id');
     return response;
   }
 
@@ -80,7 +85,8 @@ class LocationRepo implements LocationRepoInterface {
 
   @override
   Future<Response> get(String? id) async {
-    Response response = await apiClient.getData('${AppConstants.placeDetailsUri}?placeid=$id');
+    Response response =
+        await apiClient.getData('${AppConstants.placeDetailsUri}?placeid=$id');
     return response;
   }
 
@@ -88,5 +94,4 @@ class LocationRepo implements LocationRepoInterface {
   Future update(Map<String, dynamic> body, int? id) {
     throw UnimplementedError();
   }
-
 }
